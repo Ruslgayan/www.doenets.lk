76c25264760a82878d828fb825417301c6b93a4f
Ruslgayan <l.h.m.gayan@gmail.com>
2019-07-07 10:19:29 +0530

    Resykt System

---
+++ www.doenets.lk/DatabaseCon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace www.doenets.lk
+{
+    class DatabaseCon
+    {
+        public static MySqlConnection connectDB()
+        {
+            string server = "localhost";
+            string database = "doenets";
+            string uid = "root";
+            string pwd = "";
+
+            string conString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + pwd + ";";
+
+            MySqlConnection con = new MySqlConnection(conString);
+            return con;
+        }
+    }
+}
---
+++ www.doenets.lk/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace www.doenets.lk
+{
+    public partial class Welocme : Form
+    {
+        public Welocme()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //store in index number
+                string index =  txtIndex.Text;
+                int a = int.Parse(index);
+
+                Results rs = new Results();
+                rs.Show();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Please enter Valid index number!","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+
+            
+        }
+    }
+}
